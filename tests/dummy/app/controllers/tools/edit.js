0a4472d866ed8c7e4e38bef33b0cf7ff72545cca
Will Bagby <bagsbee@gmail.com>
2019-08-14 10:02:11 -0400

    fix eslint errors

--- tests/dummy/app/controllers/tools/edit.js
+++ tests/dummy/app/controllers/tools/edit.js
@@ -1,7 +1,13 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
+import { action, computed } from '@ember/object';
+import { htmlSafe } from '@ember/template';
 
 export default class ToolsEditController extends Controller {
+  @computed('model', 'model.imageUrl')
+  get backgroundImageUrl() {
+    return htmlSafe(`background-image: url(${this.model.imageUrl})`);
+  }
+
   @action
   async save() {
     await this.model.save();
