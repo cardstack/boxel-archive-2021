166e09220d72cff141ff3e58d16e82257a525502
Edward Faulkner <ef@alum.mit.edu>
2019-08-29 15:25:07 -0400

    Making cards demo animate correctly
    Co-authored-by: Will Bagby <bagsbee@gmail.com>
    Co-authored-by: Burcu Noyan <burieberry@users.noreply.github.com>

--- tests/dummy/app/controllers/cards.js
+++ tests/dummy/app/controllers/cards.js
@@ -1,10 +1,28 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
+import { action, set } from '@ember/object';
+import move from 'ember-animated/motions/move';
+import resize from 'ember-animated/motions/resize';
+import adjustCSS from 'ember-animated/motions/adjust-css';
+import { printSprites } from 'ember-animated';
+
+const borderRadius = adjustCSS.property('border-radius');
 
 export default class CardsController extends Controller {
-  queryParams = ['cardNumber'];
 
-  @action expand(id) {
-    this.transitionToRoute({ queryParams: { cardNumber: id }});
+  @action toggle(id) {
+    for (let elt of this.model) {
+      if (elt.id === id) {
+        set(elt, 'expanded', !elt.expanded);
+      } else {
+        set(elt, 'expanded', false);
+      }
+    }
+  }
+
+  * transition ({ keptSprites }) {
+    printSprites(arguments[0]);
+    keptSprites.forEach(move);
+    keptSprites.forEach(resize);
+    keptSprites.forEach(borderRadius);
   }
 }
