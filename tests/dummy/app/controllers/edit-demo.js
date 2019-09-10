92bbfcfb40c723dd78f7bfba66d59ea7918cdacd
Will Bagby <bagsbee@gmail.com>
2019-09-10 09:48:45 -0400

    WIP give mode to model

--- tests/dummy/app/controllers/edit-demo.js
+++ tests/dummy/app/controllers/edit-demo.js
@@ -1,10 +1,44 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import resize from 'ember-animated/motions/resize';
+import adjustCSS from 'ember-animated/motions/adjust-css';
+import adjustColor from 'ember-animated/motions/adjust-color';
+import { printSprites } from 'ember-animated';
+import { easeOut } from 'ember-animated/easings/cosine';
 
 export default class EditDemoController extends Controller {
-  editing = false;
+  mode = 'view';
 
-  @action toggleEdit() {
-    this.set('editing', !this.editing);
+  @action setMode(mode) {
+    this.model.set('mode', mode);
+  }
+
+
+  * trayTransition ({ keptSprites }) {
+    keptSprites.forEach(sprite => {
+      adjustColor('background-color', sprite, { easing: easeOut });
+    });
+  }
+
+  * cardTransition ({ keptSprites }) {
+    printSprites(arguments[0], 'cardTransition');
+    keptSprites.forEach(sprite => {
+      adjustColor('border-color', sprite, { easing: easeOut });
+      adjustCSS('border-radius', sprite, { easing: easeOut });
+    });
+  }
+
+  * headerTransition ({ keptSprites }) {
+    printSprites(arguments[0], 'headerTransition');
+
+    keptSprites.forEach(sprite => {
+      resize(sprite, { easing: easeOut });
+    });
+  }
+  * bodyTransition ({ keptSprites }) {
+    printSprites(arguments[0], 'bodyTransition');
+    keptSprites.forEach(sprite => {
+      adjustColor('background-color', sprite, { easing: easeOut });
+    });
   }
 }
--- tests/dummy/app/routes/edit-demo.js
+++ tests/dummy/app/routes/edit-demo.js
@@ -3,12 +3,13 @@ import BoxelizedRoute from 'boxel/routes/boxelized';
 export default class CardsRoute extends BoxelizedRoute {
   boxelPlane = 'space';
 
-  model() {
-    return {
-      id : "1",
-      title: 'Rethinking the Web 3.0 Experience',
-      description: 'What would improve the Web beyond “decentralization”?',
-      body: 'For many of us who have already played with blockchains, there’s a deep belief that Web 3.0, or “the decentralized Internet,” is primarily a network of value, with trustless, verifiable transactions for digital assets, synchronized around global ledgers.'
-    };
+  async model() {
+    return this.store.findRecord('article', 'sample');
+    // return {
+    //   id : "1",
+    //   title: 'Rethinking the Web 3.0 Experience',
+    //   description: 'What would improve the Web beyond “decentralization”?',
+    //   body: 'For many of us who have already played with blockchains, there’s a deep belief that Web 3.0, or “the decentralized Internet,” is primarily a network of value, with trustless, verifiable transactions for digital assets, synchronized around global ledgers.'
+    // };
   }
 }
