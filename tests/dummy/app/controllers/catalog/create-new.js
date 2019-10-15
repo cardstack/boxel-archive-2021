878ef2ecdde9800c7bd2c5d2d6be492b7de1e60d
Burcu Noyan <burcunoyan@gmail.com>
2019-10-15 18:24:24 -0400

    Animate transition

--- tests/dummy/app/controllers/catalog/create-new.js
+++ tests/dummy/app/controllers/catalog/create-new.js
@@ -1,5 +1,8 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import scale from 'ember-animated/motions/scale';
+import move from 'ember-animated/motions/move';
+import { printSprites, parallel } from 'ember-animated';
 
 export default class CatalogCreateNewController extends Controller {
   @action
@@ -12,5 +15,21 @@ export default class CatalogCreateNewController extends Controller {
   hideDetailView() {
     this.set('displayDetailView', false);
     this.set('selectedField', null);
+  }
+
+  * transition ({ insertedSprites, removedSprites, beacons }) {
+    printSprites(arguments[0]);
+
+    insertedSprites.forEach(sprite => {
+      sprite.startAtSprite(beacons.field);
+      parallel(move(sprite, scale(sprite)));
+      sprite.applyStyles({ 'z-index': 3 });
+    });
+
+    removedSprites.forEach(sprite => {
+      sprite.endAtSprite(beacons.field);
+      parallel(move(sprite, scale(sprite)));
+      sprite.applyStyles({ 'z-index': 3 });
+    });
   }
 }
