94ec0381e27227b046a5eb29aa63df7e2cb53235
Burcu Noyan <burcunoyan@gmail.com>
2019-10-16 11:23:36 -0400

    Transition background opacity

--- tests/dummy/app/controllers/catalog/create-new.js
+++ tests/dummy/app/controllers/catalog/create-new.js
@@ -2,13 +2,20 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import scale from 'ember-animated/motions/scale';
 import move from 'ember-animated/motions/move';
+import { default as opacity } from 'ember-animated/motions/opacity';
 import { printSprites, parallel } from 'ember-animated';
 
 export default class CatalogCreateNewController extends Controller {
   @action
-  showDetailView(field) {
-    this.set('displayDetailView', true);
-    this.set('selectedField', field);
+  toggleDetailView(field) {
+    this.toggleProperty('displayDetailView');
+
+    if (this.displayDetailView) {
+      this.set('selectedField', field);
+    }
+    else {
+      this.set('selectedField', null);
+    }
   }
 
   @action
@@ -30,6 +37,18 @@ export default class CatalogCreateNewController extends Controller {
       sprite.endAtSprite(beacons.field);
       parallel(move(sprite, scale(sprite)));
       sprite.applyStyles({ 'z-index': 3 });
+    });
+  }
+
+  * backgroundTransition ({ insertedSprites, removedSprites }) {
+    printSprites(arguments[0]);
+
+    insertedSprites.forEach(sprite => {
+      opacity(sprite, { from: 0.1 });
+    });
+
+    removedSprites.forEach(sprite => {
+      opacity(sprite, { to: 0.1 });
     });
   }
 }
