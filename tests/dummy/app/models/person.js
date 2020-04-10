a6187385e6939dc58afa673b18efa05066caca98
Burcu Noyan <burcunoyan@gmail.com>
2020-04-09 20:42:43 -0400

    Render cast collection

--- tests/dummy/app/models/person.js
+++ tests/dummy/app/models/person.js
@@ -1,13 +1,11 @@
 import Model, { attr } from '@ember-data/model';
-import { tracked } from '@glimmer/tracking';
 
 export default class Person extends Model {
   @attr() firstName;
   @attr() lastName;
+  @attr('boolean') isActor;
   @attr() role;
-
-  @tracked firstName;
-  @tracked lastName;
+  @attr() profile;
 
   get fullName() {
     return `${this.firstName} ${this.lastName}`;
