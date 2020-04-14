03fcb1842a225a4f9b00cb060758a28f78b3f10e
Burcu Noyan <burcunoyan@gmail.com>
2020-04-14 17:04:42 -0400

    Simplify item selection

--- tests/dummy/app/components/collection.js
+++ tests/dummy/app/components/collection.js
@@ -4,12 +4,10 @@ import { action, set } from '@ember/object';
 
 export default class CollectionComponent extends Component {
   @tracked listSelected = false;
-  @tracked itemSelected = false;
 
   @action
   listSelect() {
     this.listSelected = true;
-    this.itemSelected = false;
   }
 
   @action
@@ -18,21 +16,13 @@ export default class CollectionComponent extends Component {
   }
 
   @action
-  itemSelect(id, list) {
-    for (let item of list) {
-      if (item.id === id) {
-        set(item, "selected", true);
-        this.listSelected = false;
-      }
-    }
+  itemSelect(item) {
+    set(item, "selected", true);
+    this.listSelected = false;
   }
 
   @action
-  itemUnselect(id, list) {
-    for (let item of list) {
-      if (item.id === id) {
-        set(item, "selected", false);
-      }
-    }
+  itemUnselect(item) {
+    set(item, "selected", false);
   }
 }
