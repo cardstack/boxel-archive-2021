4c56c1c5445ce961e5a843f3904b2b25a4066106
Burcu Noyan <burcunoyan@gmail.com>
2020-04-24 17:58:26 -0400

    Clean up

--- addon/components/collection-table.js
+++ addon/components/collection-table.js
@@ -2,7 +2,5 @@ import CollectionIsolated from './collection-isolated';
 import { tracked } from '@glimmer/tracking';
 
 export default class CollectionTableComponent extends CollectionIsolated {
-  rowSelectionMode = "single";
-  @tracked selection;
   @tracked sorts;
 }
