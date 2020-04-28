a36cbcb684d1041c3478f89b13a001469f089f48
Burcu Noyan <burcunoyan@gmail.com>
2020-04-28 12:48:43 -0400

    Display versions and detail

--- tests/dummy/app/routes/movie-registry/versions.js
+++ tests/dummy/app/routes/movie-registry/versions.js
@@ -1,4 +1,13 @@
-import MovieRoute from './movie-route';
+import Route from '@ember/routing/route';
 
-export default class MovieRegistryVersionsRoute extends MovieRoute {
+export default class MovieRegistryVersionsRoute extends Route {
+  model({ id }) {
+    let { versioning, movies } = this.modelFor('movie-registry');
+    let { versions } = versioning.filter(item => item.id === id)[0];
+    let movie = movies.filter(item => item.id === id)[0];
+    return {
+      versions,
+      movie
+    }
+  }
 }
