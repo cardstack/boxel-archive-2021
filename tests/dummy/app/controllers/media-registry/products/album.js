9ff82a8348c6e9a91bf7bb3c4fa82c1596a40dad
Burcu Noyan <burcunoyan@gmail.com>
2020-07-28 20:27:51 -0400

    Add embedded table

--- tests/dummy/app/controllers/media-registry/products/album.js
+++ tests/dummy/app/controllers/media-registry/products/album.js
@@ -1,4 +1,5 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
 export default class MediaRegistryProductsAlbumController extends Controller {
   get headerDetailFields() {
@@ -17,5 +18,77 @@ export default class MediaRegistryProductsAlbumController extends Controller {
         value: this.model.tracks.length
       }
     ];
+  }
+
+  get fields() {
+    if (!this.model || !this.model.tracks) { return null; }
+
+    let totalDuration = this.model.tracks.map(el => {
+      let [ m, s ] = el.length.split(':');
+      return m * 60 + Number(s);
+    }).reduce((acc, cur) => acc + cur);
+
+    let minutes = Math.floor(totalDuration / 60);
+    let seconds = totalDuration - (minutes * 60);
+    let formattedTotalDuration = [ minutes, seconds ].join(':');
+
+    return [
+      {
+        type: 'collection',
+        format: 'table',
+        value: {
+          rows: this.model.tracks,
+          columns: [
+            {
+              name: 'Track No.',
+              valuePath: 'track_no',
+              width: 125,
+              isFixed: 'left',
+              textAlign: 'right'
+            },
+            {
+              name: 'Title',
+              valuePath: 'song_title',
+              width: 290,
+              titleCase: true,
+              hasAudio: true
+            },
+            {
+              name: 'Duration',
+              valuePath: 'length',
+              width: 145,
+              textAlign: 'right'
+            },
+            {
+              name: 'Artist',
+              valuePath: 'artist',
+              width: 235
+            },
+            {
+              name: 'Artwork',
+              valuePath: 'cover_art',
+              width: 235,
+              type: 'image'
+            },
+            {
+              name: 'Genre',
+              valuePath: 'genre',
+              width: 235
+            },
+          ],
+          footerRows: [
+            {
+              track_no: `${this.model.tracks.length}`,
+              length: `${formattedTotalDuration}`
+            }
+          ]
+        }
+      }
+    ]
+  }
+
+  @action
+  transitionToCatalog(id) {
+    this.transitionToRoute('media-registry.collection', id);
   }
 }
--- tests/dummy/app/routes/media-registry/products/album.js
+++ tests/dummy/app/routes/media-registry/products/album.js
@@ -5,9 +5,50 @@ import { formatId } from '@cardstack/boxel/utils/format-id';
 export default class MediaRegistryProductsAlbumRoute extends Route {
   async model({ albumId }) {
     const tracks = await fetchCollection('all_tracks_combined');
+    const musicalWorks = await fetchCollection('musical-works');
+    const allCollections = this.modelFor('media-registry')?.collection;
+
     const albumTracks = tracks.filter(el => formatId(el.album) === albumId);
-    let album = albumTracks[0];
-    album.tracks = albumTracks;
-    return album;
+
+    let model = {};
+
+    let {
+      album,
+      artist,
+      catalog,
+      cover_art,
+      cover_art_date,
+      cover_art_large,
+      cover_art_medium,
+      cover_art_thumb,
+      genre,
+      owner,
+      type_of_album
+    } = albumTracks[0];
+
+    model = {
+      album,
+      artist,
+      catalog,
+      cover_art,
+      cover_art_date,
+      cover_art_large,
+      cover_art_medium,
+      cover_art_thumb,
+      genre,
+      owner,
+      type_of_album
+    };
+
+    model.tracks = albumTracks;
+
+    model.tracks.map((el, i) => {
+      el.musicalWork = musicalWorks.find(item => formatId(item.title) === formatId(el.song_title));
+      el.track_no = String(i + 1);
+    });
+
+    model.collections = allCollections.filter(el => model.catalog.includes(el.catalog_title));
+
+    return model;
   }
 }
