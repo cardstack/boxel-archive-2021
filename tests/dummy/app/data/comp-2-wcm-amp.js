235da930ad1eec6f2fe1133195fc0a5873a794c0
Burcu Noyan <burcunoyan@gmail.com>
2020-08-04 12:06:49 -0400

    Add sample data for second example

--- tests/dummy/app/data/comp-2-wcm-amp.js
+++ tests/dummy/app/data/comp-2-wcm-amp.js
@@ -1,64 +1,8 @@
-import { VALENTINO_SOLANO, EMILIO_ROSSO } from './profiles';
+import { VALENTINO_SOLANO, EMILIO_ROSSO, MARIA_BIANCHI } from './profiles';
 
 export default {
-  id: "mw-the-sun-comes-out-wmg-wcm",
+  id: "the-sun-comes-out-wcm-amp",
   baseCard: {
-    type: 'musical-work',
-    owner: 'Warner Music Group',
-    datetime: '2020-04-08T10:45',
-    id: 'wmg-the-sun-comes-out',
-    itemId: 'wmg-the-sun-comes-out',
-    title: 'The Sun Comes Out',
-    isolatedFields: [
-      {
-        title: "title",
-        value: "The Sun Comes Out"
-      },
-      {
-        title: "writers",
-        value: [
-          {
-            title: 'Lyricist',
-            type: 'collection',
-            component: 'cards/composer',
-            value: [ VALENTINO_SOLANO ]
-          }
-        ]
-      },
-      {
-        title: 'iswc',
-        value: "T-070237182-9"
-      },
-      {
-        title: 'verifi_id',
-        id: '0x2b4932f7c27d6ca5d8bd5601ba7c28071221165ac2f1b7928c22c2809d24183ca',
-        type: 'card',
-        component: 'cards/registration-embedded',
-        value: {
-          verifi_id: '0x2b4932f7c27d6ca5d8bd5601ba7c28071221165ac2f1b7928c22c2809d24183ca',
-          verifi_reg_date: '2020-04-08',
-          asset_type: 'Original work'
-        }
-      },
-      {
-        title: 'version_type',
-        value: null
-      },
-      {
-        title: 'ownership_splits',
-        value: null
-      },
-      {
-        title: 'publisher',
-        value: null
-      },
-      {
-        title: 'copyright_notice',
-        value: null
-      }
-    ]
-  },
-  compCard: {
     type: 'musical-work',
     owner: 'Warner Chappell Music',
     datetime: '2019-11-11T13:54',
@@ -109,15 +53,120 @@ export default {
       {
         title: 'ownership_splits',
         value: [
-            {
-              title: 'Valentino Solano (Lyricist)',
-              value: '50%'
-            },
-            {
-              title: 'Emilio Rosso (Composer)',
-              value: '50%'
-            }
-          ]
+          {
+            title: 'Valentino Solano (Lyricist)',
+            value: '50%'
+          },
+          {
+            title: 'Emilio Rosso (Composer)',
+            value: '50%'
+          }
+        ]
+      },
+      {
+        title: 'publisher',
+        type: 'collection',
+        value: [
+          {
+            type: 'publisher',
+            title: 'Warner Chappel Music',
+            fields: [
+              {
+                title: 'website',
+                value: 'www.warnerchappell.com'
+              },
+              {
+                title: 'main office',
+                value: 'Los Angeles, USA'
+              }
+            ]
+          },
+          {
+            type: 'publisher',
+            title: 'Allegro Music Publishing',
+            fields: [
+              {
+                title: 'website',
+                value: 'www.allegromusic.com'
+              },
+              {
+                title: 'main office',
+                value: 'New York, USA'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        title: 'copyright_notice',
+        value: ['© 2019 Warner Chappell Music', '© 2019 Allegro Music Publishing']
+      }
+    ]
+  },
+  compCard: {
+    type: 'musical-work',
+    owner: 'Allegro Music Publishing',
+    datetime: '2020-05-18T11:36',
+    id: 'amp-the-sun-comes-out',
+    itemId: 'amp-the-sun-comes-out',
+    title: 'The Sun Comes Out',
+    isolatedFields: [
+      {
+        title: "title",
+        value: "The Sun Comes Out"
+      },
+      {
+        title: "writers",
+        value: [
+          {
+            title: 'Lyricist',
+            type: 'collection',
+            component: 'cards/composer',
+            value: [ VALENTINO_SOLANO ]
+          },
+          {
+            title: 'Composers',
+            type: 'collection',
+            component: 'cards/composer',
+            value: [ EMILIO_ROSSO, MARIA_BIANCHI ]
+          }
+        ]
+      },
+      {
+        title: 'iswc',
+        value: "T-070237182-9"
+      },
+      {
+        title: 'verifi_id',
+        id: '0x31ef32b4c27f6ca5d6bd6201fa7c14071228965dc2f1b4328c22e5609e8912ab4',
+        type: 'card',
+        component: 'cards/registration-embedded',
+        value: {
+          verifi_id: '0x31ef32b4c27f6ca5d6bd6201fa7c14071228965dc2f1b4328c22e5609e8912ab4',
+          verifi_reg_date: '2020-05-18',
+          asset_type: 'Original work'
+        }
+      },
+      {
+        title: 'version_type',
+        value: 'Original work'
+      },
+      {
+        title: 'ownership_splits',
+        value: [
+          {
+            title: 'Valentino Solano (Lyricist)',
+            value: '50%'
+          },
+          {
+            title: 'Emilio Rosso (Composer)',
+            value: '25%'
+          },
+          {
+            title: 'Maria Bianchi (Composer)',
+            value: '25%'
+          }
+        ]
       },
       {
         title: 'publisher',
--- tests/dummy/app/data/discrepancies-list.js
+++ tests/dummy/app/data/discrepancies-list.js
@@ -1,5 +1,7 @@
 import COMP1 from './comp-1-wmg-wcm';
+import COMP2 from './comp-2-wcm-amp';
 
 export default [
-  COMP1
+  COMP1,
+  COMP2
 ]
