22063ff34b7b9f9231f8d0b5a4884374dda9362d
Burcu Noyan <burcunoyan@gmail.com>
2020-08-04 16:57:41 -0400

    Display added cards and modified values

--- tests/dummy/app/controllers/media-registry/discrepancies/discrepancy.js
+++ tests/dummy/app/controllers/media-registry/discrepancies/discrepancy.js
@@ -1,40 +1,88 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { typeOf } from '@ember/utils';
 
 export default class MediaRegistryDiscrepanciesDiscrepancyController extends Controller {
   @tracked addedValues = [];
-  @tracked removedModifiedValues = [];
   @tracked changedFields = [];
   @tracked changedCards = [];
   @tracked addedCards = [];
   @tracked removedCards = [];
+  @tracked modifiedValues = [];
   @tracked modifiedCard = {};
 
   @action
+  reset() {
+    this.addedValues = [];
+    this.changedFields = [];
+    this.changedCards = [];
+    this.addedCards = [];
+    this.removedCards = [];
+    this.modifiedValues = [];
+    this.modifiedCard = {};
+  }
+
+  @action
   compareFields(field, compField) {
+    let [ json1, json2 ] = [ field, compField ].map(data => JSON.stringify(data));
+
+    if (json1 === json2) {
+      return;
+    }
+
+    // added field value
     if (!field.value && compField.value) {
-      this.addedValues.push(compField.title);
+      return this.addedValues.push(compField.title);
     }
 
-    // using 'removedModifiedValues' instead of 'removedValues'
-    else if (field.value && !compField.value) {
-      this.removedModifiedValues.push(field.title);
+    // removed field value
+    if (field.value && !compField.value) {
+      return this.removedValues.push(field.title);
     }
 
-    else if (field.type === 'card' && compField.type === 'card') {
-      this.modifiedCard = {
+    if (compField.type === 'card') {
+      return this.modifiedCard = {
         value: compField.value,
         oldValue: field.value
       };
     }
 
-    else {
-      this.changedFields.push({
-        title: field.title,
-        oldValue: field.value,
-        value: compField.value
-      });
+    if (compField.type === 'collection') {
+      return;
     }
+
+    if (typeOf(compField.value) === 'array') {
+      let fieldJSON = JSON.stringify(field);
+
+      for (let val of compField.value) {
+        let valJSON = JSON.stringify(val);
+
+        if (val.type === 'card') {
+          if (!fieldJSON.includes(valJSON)) {
+            this.addedCards.push(val);
+          }
+        }
+        else if (val.type === 'collection') {
+          for (let v of val.value) {
+            let vJSON = JSON.stringify(v);
+            if (!fieldJSON.includes(vJSON)) {
+              this.addedCards.push(v);
+            }
+          }
+        }
+        else {
+          this.modifiedValues.push(field.title);
+        }
+      }
+
+      if (this.addedCards.length || this.modifiedValues.length) {
+        return 'hasValue';
+      } else {
+        return;
+      }
+    }
+
+    return;
   }
 }
