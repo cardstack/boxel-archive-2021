9a3ee411befa3d3bc6aa71f73e6fdf03f676edd3
Burcu Noyan <burcunoyan@gmail.com>
2020-09-01 13:09:38 -0400

    Add sample users, orgs, and fetch data

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -8,6 +8,7 @@ class Router extends EmberRouterScroll {
 }
 
 Router.map(function() {
+  this.route('workflow');
   this.route('home', { path: 'media-registry' });
   this.route('media-registry', { path: '/media-registry/:orgId' }, function() {
     this.route('collection', { path: '/collections/:collectionId' }, function() {
