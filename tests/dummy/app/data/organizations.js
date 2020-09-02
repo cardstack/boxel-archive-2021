f32c091752ca99e6e0a76b36aa3b493f8c7796db
Burcu Noyan <burcunoyan@gmail.com>
2020-09-02 15:06:35 -0400

    Cardflow fixes

--- tests/dummy/app/data/organizations.js
+++ tests/dummy/app/data/organizations.js
@@ -9,74 +9,74 @@ export default [
         title: "Lisa Track",
         role: "Administrator",
         imgURL: "/media-registry/profiles/Lisa-Track.jpg",
-        queueCards: [
-          {
-            id: "1",
-            status: "unread",
-            title: "Catalog transfer",
-            description: "Catalog transfer from Bunny Records to CRD Records for 16 masters",
-            datetime: "2020-08-31T14:46",
-            projectTitle: "Rights transfer, CRD Records",
-            progressPct: 20
-          },
-          {
-            id: "2",
-            status: "needs-response",
-            title: "Interesting band | Jelly Club",
-            datetime: "2020-08-31T13:26",
-            projectTitle: "Potential artist, Hard rock",
-            progressPct: 40,
-          },
-          {
-            id: "3",
-            status: "needs-response",
-            title: "Cover art | GG Greatest Hits",
-            datetime: "2020-08-25T17:02",
-            projectTitle: "New album, Golden Girl",
-            progressPct: 40,
-          },
-          {
-            id: "4",
-            status: "closed",
-            title: "Agreements | BB Clarke",
-            datetime: "2020-08-20T15:11",
-            projectTitle: "Rights transfer, CRD Records",
-            progressPct: 100,
-          },
-          {
-            id: "5",
-            status: "closed",
-            title: "Agreements | Pia Midina",
-            datetime: "2020-07-31T11:40",
-            projectTitle: "Rights transfer, CRD Records",
-            progressPct: 100,
-          },
-          {
-            id: "6",
-            status: "recent",
-            title: "Registration | GG Greatest Hits",
-            datetime: "2020-07-30T09:22",
-            projectTitle: " New album, Golden Girl",
-            progressPct: 40,
-          },
-          {
-            id: "7",
-            status: "closed",
-            title: "Request | Pia Midina",
-            datetime: "2020-07-16T13:38",
-            projectTitle: "Rights transfer, CRD Records",
-            progressPct: 100,
-          },
-          {
-            id: "8",
-            status: "recent",
-            title: "Tracks | GG Greatest Hits",
-            datetime: "2020-07-15T12:21",
-            projectTitle: " New album, Golden Girl",
-            progressPct: 40
-          }
-        ]
-      }
+      },
+      queueCards: [
+        {
+          id: "1",
+          status: "unread",
+          label: "Catalog transfer",
+          description: "Catalog transfer from Bunny Records to CRD Records for 16 masters",
+          datetime: "2020-08-31T14:46",
+          title: "Rights transfer, CRD Records",
+          progressPct: 20
+        },
+        {
+          id: "2",
+          status: "needs-response",
+          label: "Interesting band | Jelly Club",
+          datetime: "2020-08-31T13:26",
+          title: "Potential artist, Hard rock",
+          progressPct: 40,
+        },
+        {
+          id: "3",
+          status: "needs-response",
+          label: "Cover art | GG Greatest Hits",
+          datetime: "2020-08-25T17:02",
+          title: "New album, Golden Girl",
+          progressPct: 40,
+        },
+        {
+          id: "4",
+          status: "closed",
+          label: "Agreements | BB Clarke",
+          datetime: "2020-08-20T15:11",
+          title: "Rights transfer, CRD Records",
+          progressPct: 100,
+        },
+        {
+          id: "5",
+          status: "closed",
+          label: "Agreements | Pia Midina",
+          datetime: "2020-07-31T11:40",
+          title: "Rights transfer, CRD Records",
+          progressPct: 100,
+        },
+        {
+          id: "6",
+          status: "recent",
+          label: "Registration | GG Greatest Hits",
+          datetime: "2020-07-30T09:22",
+          title: " New album, Golden Girl",
+          progressPct: 40,
+        },
+        {
+          id: "7",
+          status: "closed",
+          label: "Request | Pia Midina",
+          datetime: "2020-07-16T13:38",
+          title: "Rights transfer, CRD Records",
+          progressPct: 100,
+        },
+        {
+          id: "8",
+          status: "recent",
+          label: "Tracks | GG Greatest Hits",
+          datetime: "2020-07-15T12:21",
+          title: " New album, Golden Girl",
+          progressPct: 40
+        }
+      ]
     },
     {
       id: 'crd_records',
@@ -87,51 +87,51 @@ export default [
       user: {
         title: "Steve Rights",
         role: "Catalog Manager",
-        imgURL: "/media-registry/profiles/Steve-Rights.jpg",
-        queueCards: [
-          {
-            id: "1",
-            status: "open",
-            title: "Catalog transfer",
-            description: "Catalog transfer from Bunny Records to CRD Records for 16 masters",
-            datetime: "2020-08-31T14:56",
-            projectTitle: "Rights transfer, Bunny Records",
-            progressPct: 40
-          },
-          {
-            id: "2",
-            status: "needs-response",
-            title: "Radio interview | FM8",
-            datetime: "2020-08-25T11:00",
-            projectTitle: "Interviews, CEO",
-            progressPct: 20
-          },
-          {
-            id: "3",
-            status: "closed",
-            title: "To Dos",
-            datetime: "2020-08-13T10:10",
-            projectTitle: "Rights transfer, Bunny Records",
-            progressPct: 100
-          },
-          {
-            id: "4",
-            status: "recent",
-            title: "TV interview | ZBZ",
-            datetime: "2020-08-12T16:38",
-            projectTitle: "Interviews, CEO",
-            progressPct: 40
-          },
-          {
-            id: "5",
-            status: "recent",
-            title: "Newspaper interview | New Times",
-            datetime: "2020-07-29T17:01",
-            projectTitle: "Interviews, CEO",
-            progressPct: 40
-          }
-        ]
-      }
+        imgURL: "/media-registry/profiles/Steve-Rights.jpg"
+      },
+      queueCards: [
+        {
+          id: "1",
+          status: "unread",
+          label: "Catalog transfer",
+          description: "Catalog transfer from Bunny Records to CRD Records for 16 masters",
+          datetime: "2020-08-31T14:56",
+          title: "Rights transfer, Bunny Records",
+          progressPct: 40
+        },
+        {
+          id: "2",
+          status: "needs-response",
+          label: "Radio interview | FM8",
+          datetime: "2020-08-25T11:00",
+          title: "Interviews, CEO",
+          progressPct: 20
+        },
+        {
+          id: "3",
+          status: "closed",
+          label: "To Dos",
+          datetime: "2020-08-13T10:10",
+          title: "Rights transfer, Bunny Records",
+          progressPct: 100
+        },
+        {
+          id: "4",
+          status: "recent",
+          label: "TV interview | ZBZ",
+          datetime: "2020-08-12T16:38",
+          title: "Interviews, CEO",
+          progressPct: 40
+        },
+        {
+          id: "5",
+          status: "recent",
+          label: "Newspaper interview | New Times",
+          datetime: "2020-07-29T17:01",
+          title: "Interviews, CEO",
+          progressPct: 40
+        }
+      ]
     },
     {
       id: 'warner-music-group',
