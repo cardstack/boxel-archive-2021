f17f723376c892c5fd759ad16c018a718747b79a
Burcu Noyan <burcunoyan@gmail.com>
2020-09-08 18:03:31 -0400

    Fix participants

--- addon/components/cardflow.js
+++ addon/components/cardflow.js
@@ -7,8 +7,34 @@ export default class CardflowComponent extends Component {
   @tracked actionSteps = this.args.actionSteps;
   @tracked lastUpdated = this.args.lastUpdated;
 
-  get project() {
-    return this.args.model;
+  get thread() {
+    return this.args.model.thread;
+  }
+
+  get user() {
+    return this.args.model.user;
+  }
+
+  get participants() {
+    return this.args.model.participants;
+  }
+
+  get org() {
+    return this.args.model.currentOrg;
+  }
+
+  get participatingOrgMembers() {
+    if (!this.participants || !this.participants.length) { return null; }
+    if (!this.org) { return null; }
+    return this.participants.filter(el => el.org_ids && el.org_ids.includes(this.org.id));
+  }
+
+  get otherParticipants() {
+    if (!this.participants) { return null; }
+    if (!this.participatingOrgMembers || !this.participatingOrgMembers.length) {
+      return this.participants;
+    }
+    return this.participants.filter(el => !el.org_ids || !el.org_ids.includes(this.org.id));
   }
 
   @action
--- tests/dummy/app/routes/workflow/org/thread.js
+++ tests/dummy/app/routes/workflow/org/thread.js
@@ -2,7 +2,27 @@ import Route from '@ember/routing/route';
 
 export default class WorkflowOrgThreadRoute extends Route {
   async model({ threadId }) {
-    const { orgQueueCards } = this.modelFor('workflow.org');
-    return orgQueueCards.find(el => el.id === threadId);
+    const { user, currentOrg, orgQueueCards } = this.modelFor('workflow.org');
+    let thread = orgQueueCards.find(el => el.id === threadId);
+    let participants = [];
+
+    if (thread.participant_ids && thread.participant_ids.length) {
+      const { users } = this.modelFor('workflow');
+      for (const userId of thread.participant_ids) {
+        let participant = users.find(el => el.id === userId);
+        if (participant) {
+          participants = [...participants, participant];
+        } else {
+          participants = [...participants, { id: userId, type: "participant", title: userId } ];
+        }
+      }
+    }
+
+    return {
+      user,
+      thread,
+      participants,
+      currentOrg
+    }
   }
 }
