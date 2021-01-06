ae7e61a4fbf8e1a708fc972157a1cfb79799faec
Matt McManus <matt@mcmanus.io>
2021-01-06 12:05:11 -0500

    Improve build speed by not fingerprinting assets in dev

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,9 +1,15 @@
 'use strict';
+const crypto = require('crypto');
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const environment = EmberApp.env();
+const IS_PROD = environment === 'production';
+const IS_TEST = environment === 'test';
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    hinting: false, // Don't lint at build time
+    tests: IS_TEST, // Only generate tests files in when running `ember test`
     /*
       Leave jQuery out of this addon's own test suite & dummy app by default,
       so that the addon can be used in apps without jQuery. If you really need
@@ -20,12 +26,22 @@ module.exports = function(defaults) {
       fingerprintAssetMap: true,
       prepend: '/boxel/',
       replaceExtensions: ['html', 'css', 'js', 'json'],
-      enabled: (process.env.EMBER_ENV !== 'test')
+      enabled: !IS_TEST,
+      customHash: function(buf) {
+        if (IS_PROD) {
+          let md5 = crypto.createHash('md5');
+          md5.update(buf);
+          return md5.digest('hex');
+        }
+
+        return '0';
+      }
     },
 
     // Add options here
     'ember-power-select': { theme: false }
   });
+
 
   return app.toTree();
 };
