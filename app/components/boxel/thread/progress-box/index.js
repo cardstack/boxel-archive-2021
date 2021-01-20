8f7e3042ecb35da87bc03a23053f689c24586c4d
Burcu Noyan <burcunoyan@gmail.com>
2021-01-20 16:13:56 -0500

    percentComplete calculation

---
+++ app/components/boxel/thread/progress-box/index.js
@@ -0,0 +1,14 @@
+import Component from '@glimmer/component';
+
+export default class ProgressBoxComponent extends Component {
+  get percentComplete() {
+    let totalMilestones = this.args.milestones ? this.args.milestones.length : undefined;
+    let completedCount = this.args.completedCount || 0;
+
+    if (!totalMilestones) {
+      return 0;
+    }
+
+    return Math.round(completedCount / totalMilestones * 100);
+  }
+}
