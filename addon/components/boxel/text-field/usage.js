487d91349472dc9cb4d1ea93678ef771eaf87916
Burcu Noyan <burcunoyan@gmail.com>
2021-02-26 13:01:49 -0500

    TextField component with  label and input

---
+++ addon/components/boxel/text-field/usage.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class extends Component {
+  @tracked label = 'Name';
+  @tracked value = 'Lila';
+
+  @action updateValue(ev) {
+    this.value = ev.target.value;
+  }
+}
