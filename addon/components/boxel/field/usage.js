0985118ae050e8557ad38239c6026bbd4f94fdec
Burcu Noyan <burcunoyan@gmail.com>
2021-03-02 19:24:45 -0500

    Use in field-renderer

--- addon/components/boxel/field/usage.js
+++ addon/components/boxel/field/usage.js
@@ -5,7 +5,7 @@ import { action } from '@ember/object';
 export default class extends Component {
   @tracked label = 'Name';
   @tracked value = 'Lila';
-  @tracked isEditMode = true;
+  @tracked labelClass;
 
   @action updateValue(ev) {
     this.value = ev.target.value;
