fd208270ebc1807b88ddf9bab9fe58bfa5be7b73
Aierie <michael.khor@cardstack.com>
2021-03-03 13:25:52 +0800

    improve freestyle usage example

--- addon/components/boxel/button/usage.js
+++ addon/components/boxel/button/usage.js
@@ -1,12 +1,27 @@
 /* eslint-disable no-console */
 import Component from '@glimmer/component';
-
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 export default class extends Component {
-  appearanceVariants = {
+  boxelFontSizes = ['xs', 'sm', '', 'lg', 'xl'].map((v) =>
+    v ? `var(--boxel-font-size-${v})` : 'var(--boxel-font-size)'
+  );
+  boxelSpacingSizes = [
+    'xxxs',
+    'xxs',
+    'xs',
+    'sm',
+    '',
+    'lg',
+    'xl',
+    'xxl',
+    'xxxl',
+  ].map((v) => (v ? `var(--boxel-sp-${v})` : 'var(--boxel-sp)'));
+  sizeVariants = ['small', 'base', 'large'];
+  kindVariants = {
     light: ['primary', 'secondary-light'],
     dark: ['primary', 'secondary-dark'],
   };
-  sizeVariants = ['small', 'base', 'large'];
   iconVariants = [
     {
       text: 'left',
@@ -24,4 +39,17 @@ export default class extends Component {
       iconRight: 'close',
     },
   ];
+
+  @tracked size = 'base';
+  @tracked kind = 'primary';
+  @tracked disabled = false;
+  @tracked iconLeft = '';
+  @tracked iconRight = '';
+  @tracked iconSpacing = 'var(--boxel-sp-xs)';
+  @tracked iconSize = 'var(--boxel-font-sm)';
+
+  @action
+  alert() {
+    alert('Hey! You clicked the button.');
+  }
 }
