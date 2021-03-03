5fec02a40c374375fff5b1aafd6a4cfb0466f573
Aierie <michael.khor@cardstack.com>
2021-03-03 14:50:06 +0800

    run menu test with data-test selectors

--- tests/integration/components/boxel/menu-test.js
+++ tests/integration/components/boxel/menu-test.js
@@ -3,9 +3,10 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-const MENU_ITEM_SELECTOR = '.menu__item';
-const MENU_SEPARATOR_SELECTOR = '.menu__separator';
-const TEST_MENU_ITEM_TEXT_ATTRIBUTE = 'data-test-menu-item';
+const MENU_ITEM_SELECTOR = '[data-test-boxel-menu-item]';
+const MENU_ITEM_ICON_SELECTOR = '[data-test-boxel-menu-item-icon]'
+const MENU_SEPARATOR_SELECTOR = '[data-test-boxel-menu-separator]';
+const TEST_MENU_ITEM_TEXT_ATTRIBUTE = 'data-test-boxel-menu-item-text';
 
 module('Integration | Component | Menu', function (hooks) {
   setupRenderingTest(hooks);
@@ -67,11 +68,11 @@ module('Integration | Component | Menu', function (hooks) {
     `);
     assert.dom(`${MENU_ITEM_SELECTOR}:nth-child(1)`).hasClass(/--dangerous/);
     assert.dom(`${MENU_ITEM_SELECTOR}:nth-child(2)`).hasClass(/--has-icon/);
-    assert.dom(`${MENU_ITEM_SELECTOR}:nth-child(2) svg`).exists();
+    assert.dom(`${MENU_ITEM_SELECTOR}:nth-child(2) ${MENU_ITEM_ICON_SELECTOR}`).exists();
     assert
       .dom(`${MENU_ITEM_SELECTOR}:nth-child(3)`)
       .hasClass(/--has-icon/)
       .hasClass(/--dangerous/);
-    assert.dom(`${MENU_ITEM_SELECTOR}:nth-child(3) svg`).exists();
+    assert.dom(`${MENU_ITEM_SELECTOR}:nth-child(3) ${MENU_ITEM_ICON_SELECTOR}`).exists();
   });
 });
