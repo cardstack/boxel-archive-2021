41057be0dc1cdcf1c22bbb5c957f31383de5c00d
Burcu Noyan <burcunoyan@gmail.com>
2021-03-03 20:12:41 -0500

    More changes

--- addon/components/boxel/field/usage.js
+++ addon/components/boxel/field/usage.js
@@ -3,9 +3,11 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class extends Component {
-  @tracked label = 'Name';
-  @tracked value = 'Lila';
+  @tracked label = 'Full Name of the Issuer';
+  @tracked value = 'Gary Walker';
+  @tracked mode = 'edit';
   @tracked labelClass;
+  @tracked labelWidth = '7.5rem';
 
   @action updateValue(ev) {
     this.value = ev.target.value;
