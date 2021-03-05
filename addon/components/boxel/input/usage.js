5e5646cffb57cde84d8cb33331146ecc10b26354
Burcu Noyan <burcunoyan@gmail.com>
2021-03-04 19:03:41 -0500

    Remove extras

--- addon/components/boxel/input/usage.js
+++ addon/components/boxel/input/usage.js
@@ -3,7 +3,6 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class extends Component {
-  @tracked size = 'default';
   @tracked id = 'sample-input';
   @tracked value = 'Hello Boxel';
   @tracked disabled = false;
