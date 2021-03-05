b991f7d53562ca6d2647c48fa280e024f7c85800
Burcu Noyan <burcunoyan@gmail.com>
2021-03-04 19:20:37 -0500

    Add data-test attributes

--- addon/components/boxel/field/usage.js
+++ addon/components/boxel/field/usage.js
@@ -6,7 +6,8 @@ export default class extends Component {
   @tracked label = 'Full Name of the Issuer';
   @tracked value = 'Gary Walker';
   @tracked mode = 'edit';
-  @tracked labelClass;
+  @tracked id = 'sample-field';
+  @tracked labelClass = 'custom-classname';
   @tracked labelWidth = '7.5rem';
 
   @action onInput(ev) {
