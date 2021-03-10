4fd1dd5ec1f8548c1e68db3d3280f3e9ccf3f496
Burcu Noyan <burcunoyan@gmail.com>
2021-03-09 20:26:40 -0500

    Input changes; add TextField

--- tests/integration/components/boxel/input-test.js
+++ tests/integration/components/boxel/input-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, fillIn, pauseTest } from '@ember/test-helpers';
+import { render, fillIn } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | Input', function (hooks) {
