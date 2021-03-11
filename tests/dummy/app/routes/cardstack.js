9845d9ffbf340ca19079fa37e5f18197c2d0e158
Burcu Noyan <burcunoyan@gmail.com>
2021-03-11 09:58:54 -0500

    Start adding card space routes and templates

---
+++ tests/dummy/app/routes/cardstack.js
@@ -0,0 +1,32 @@
+import Route from '@ember/routing/route';
+import CardstackLogo from '../../../public/@cardstack/boxel/images/icons/cardstack-logo.svg';
+
+const ORGS = [
+  {
+    id: 'card-space',
+    title: 'Card Space',
+    url: 'cardstack.card-space',
+  },
+  {
+    id: 'card-pay',
+    title: 'Card Pay',
+    url: 'cardstack.card-pay',
+  },
+  {
+    id: 'card-catalog',
+    title: 'Card Catalog',
+  },
+  {
+    id: 'card-membership',
+    title: 'Card Membership',
+  },
+];
+
+export default class CardstackRoute extends Route {
+  async model() {
+    return {
+      logo: CardstackLogo,
+      orgs: ORGS,
+    }
+  }
+}
