fca2de247cbca57d0b151c4a3fe9894156c6f5d4
Burcu Noyan <burcunoyan@gmail.com>
2021-03-12 18:42:52 -0500

    Own edges implementation

--- tests/dummy/app/controllers/cardstack.js
+++ tests/dummy/app/controllers/cardstack.js
@@ -1,10 +1,13 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { or } from 'macro-decorators';
 
 export default class CardStackController extends Controller {
   @service('cardstack-session') cardstackSession;
   @service('edges') edges;
+  @or('edges.displayLeftEdge', 'cardstackSession.isAuthenticated') displayLeftEdge;
+
 
   @action updateEdges() {
     this.edges.showLeftEdge();
