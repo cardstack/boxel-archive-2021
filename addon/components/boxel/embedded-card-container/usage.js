571eb0a6cebeff25ea353ff30dc2925b8815c42d
Burcu Noyan <burcunoyan@gmail.com>
2021-03-15 17:30:08 -0400

    Update card header component

--- addon/components/boxel/embedded-card-container/usage.js
+++ addon/components/boxel/embedded-card-container/usage.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
 export default class extends Component {
-  @tracked displayHeader = false;
+  @tracked displayHeader = true;
   @tracked header = 'Card';
   @tracked noHeaderBackground = false;
 }
