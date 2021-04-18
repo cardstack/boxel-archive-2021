380cad0deffc646ee9241fc8d198037163ff8cb9
Aierie <michael.khor@cardstack.com>
2021-04-19 02:25:22 +0800

    dayjs-from helper

---
+++ addon/helpers/dayjs-from.ts
@@ -0,0 +1,12 @@
+import dayjs from 'dayjs';
+import Helper from '@ember/component/helper';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+
+export default Helper.helper(function compute(
+  [datetime, relativeTo],
+  { withoutSuffix }: { withoutSuffix: boolean | undefined }
+) {
+  return dayjs(datetime).from(relativeTo || dayjs(), withoutSuffix);
+});
