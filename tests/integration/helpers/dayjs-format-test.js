e32e3390128337f6ad5e13da0a5eeaa0c798d504
Aierie <michael.khor@cardstack.com>
2021-04-19 13:06:18 +0800

    copy tests from ember-moment

---
+++ tests/integration/helpers/dayjs-format-test.js
@@ -0,0 +1,131 @@
+// https://github.com/stefanpenner/ember-moment/blob/master/tests/unit/helpers/moment-format-test.js
+
+import EmberObject from '@ember/object';
+import { run } from '@ember/runloop';
+import { helper } from '@ember/component/helper';
+import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from 'ember-qunit';
+
+import date from '../../helpers/date';
+
+moduleForComponent('moment-format', {
+  integration: true,
+  beforeEach() {
+    this.container.lookup('service:moment').changeLocale('en');
+  }
+});
+
+test('one arg (date)', function(assert) {
+  assert.expect(1);
+
+  this.set('date', date(date(0)));
+  this.render(hbs`{{moment-format date}}`);
+  assert.equal(this.$().text(), 'Wednesday, December 31, 1969 7:00 PM');
+});
+
+test('updating default format recomputes moment-format', function(assert) {
+  assert.expect(2);
+
+  this.set('date', date(date(0)));
+  this.render(hbs`{{moment-format date}}`);
+
+  const service = this.container.lookup('service:moment');
+
+  assert.equal(this.$().text(), 'Wednesday, December 31, 1969 7:00 PM');
+
+  run(() => {
+    service.set('defaultFormat', 'DD.MM.YYYY');
+    run.scheduleOnce('afterRender', () => {
+      assert.equal(this.$().text(), '31.12.1969');
+    });
+  });
+});
+
+test('two args (date, inputFormat)', function(assert) {
+  assert.expect(1);
+
+  this.setProperties({
+    format: 'MMMM D, YYYY',
+    date: date(Date.parse('2011-10-10T14:48:00-05:00'))
+  });
+
+  this.render(hbs`{{moment-format date format}}`);
+  assert.equal(this.$().text(), 'October 10, 2011');
+});
+
+test('three args (date, outputFormat, inputFormat)', function(assert) {
+  assert.expect(1);
+
+  this.setProperties({
+    inputFormat: 'M/D/YY',
+    outputFormat: 'MMMM D, YYYY',
+    date: '5/3/10'
+  });
+
+  this.render(hbs`{{moment-format date outputFormat inputFormat}}`);
+  assert.equal(this.$().text(), 'May 3, 2010');
+});
+
+test('change date input and change is reflected by bound helper', function(assert) {
+  assert.expect(2);
+
+  const context = EmberObject.create({
+    date: date(0)
+  });
+
+  this.set('context', context);
+
+  this.render(hbs`{{moment-format context.date}}`);
+  assert.equal(this.$().text(), 'Wednesday, December 31, 1969 7:00 PM');
+
+  run(function () {
+    context.set('date', date(60*60*24));
+  });
+
+  assert.equal(this.$().text(), 'Wednesday, December 31, 1969 7:01 PM');
+
+});
+
+test('can inline a locale instead of using global locale', function(assert) {
+  assert.expect(1);
+  this.set('date', date(date(0)));
+
+  this.render(hbs`{{moment-format date 'LLLL' locale='es'}}`);
+  assert.equal(this.$().text(), 'miércoles, 31 de diciembre de 1969 19:00');
+});
+
+test('can inline timeZone (New York)', function(assert) {
+  assert.expect(1);
+
+  this.set('date', 0);
+  this.render(hbs`{{moment-format date 'LLLL' timeZone='America/New_York'}}`);
+  assert.equal(this.$().text(), 'Wednesday, December 31, 1969 7:00 PM');
+});
+
+test('can inline timeZone (Los Angeles)', function(assert) {
+  assert.expect(1);
+
+  this.set('date', 0);
+  this.render(hbs`{{moment-format date 'LLLL' timeZone='America/Los_Angeles'}}`);
+  assert.equal(this.$().text(), 'Wednesday, December 31, 1969 4:00 PM');
+});
+
+test('can be called with null when allow-empty is set to true', function(assert) {
+  assert.expect(1);
+
+  this.set('date', null);
+  this.render(hbs`{{moment-format null allow-empty=true}}`);
+  assert.equal(this.$().text(), '');
+});
+
+test('can be called using subexpression', function(assert) {
+  assert.expect(1);
+
+  this.registry.register('helper:get-format', helper(function() {
+    return 'L';
+  }));
+
+  this.set('date', date(0));
+  this.render(hbs`{{moment-format date (get-format 'global-format')}}`);
+  assert.equal(this.$().text(), '12/31/1969');
+});
---
+++ tests/integration/helpers/dayjs-from-test.js
@@ -0,0 +1,74 @@
+// https://github.com/stefanpenner/ember-moment/blob/master/tests/unit/helpers/moment-from-test.js
+
+import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from 'ember-qunit';
+
+moduleForComponent('moment-from',{
+  integration: true,
+  beforeEach() {
+    this.container.lookup('service:moment').changeLocale('en');
+  }
+});
+
+test('one arg (date)', function(assert) {
+  assert.expect(1);
+
+  const momentService = this.container.lookup('service:moment');
+
+  this.set('date', momentService.moment().add(3, 'day'));
+
+  this.render(hbs`{{moment-from date}}`);
+  assert.equal(this.$().text(), 'in 3 days');
+});
+
+test('two args (dateA, dateB)', function(assert) {
+  assert.expect(1);
+
+  const momentService = this.container.lookup('service:moment');
+  this.setProperties({
+    dateA: new Date(),
+    dateB: momentService.moment().add(3, 'day')
+  });
+
+  this.render(hbs`{{moment-from dateB dateA}}`);
+  assert.equal(this.$().text(), 'in 3 days');
+});
+
+test('two args (dateA, dateB, hideAffix=boolean)', function(assert) {
+  assert.expect(2);
+
+  const momentService = this.container.lookup('service:moment');
+  this.setProperties({
+    dateA: new Date(),
+    dateB: momentService.moment().add(3, 'day')
+  });
+
+  this.render(hbs`{{moment-from dateB dateA hideAffix=true}}`);
+  assert.equal(this.$().text(), '3 days');
+  this.render(hbs`{{moment-from dateB dateA hideAffix=false}}`);
+  assert.equal(this.$().text(), 'in 3 days');
+});
+
+test('three args (dateA, dateB, boolean)', function(assert) {
+  assert.expect(1);
+
+  const momentService = this.container.lookup('service:moment');
+  this.setProperties({
+    dateA: new Date(),
+    dateB: momentService.moment().subtract(3, 'day')
+  });
+
+  this.render(hbs`{{moment-from dateA dateB true}}`);
+  assert.equal(this.$().text(), '3 days');
+});
+
+test('can inline a locale', function(assert) {
+  assert.expect(1);
+
+  const momentService = this.container.lookup('service:moment');
+  this.set('dateA', momentService.moment());
+  this.set('dateB', momentService.moment().subtract(2, 'day'));
+
+  this.render(hbs`{{moment-from dateA dateB locale='es'}}`);
+  assert.equal(this.$().text(), 'en 2 días');
+});
