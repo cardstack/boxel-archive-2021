9cd4cd4d7595f61439c15690012d2def970746c0
Burcu Noyan <burcunoyan@gmail.com>
2021-04-27 17:21:15 -0400

    ActionContainer changes

--- addon/components/boxel/action-container/usage.ts
+++ addon/components/boxel/action-container/usage.ts
@@ -5,6 +5,4 @@ export default class extends Component {
   @tracked isComplete = false;
   @tracked header = 'Card Header';
   @tracked prompt = 'With optional prompt';
-  @tracked incompleteActionLabel = 'Save';
-  @tracked completeActionLabel = 'Edit value';
 }
