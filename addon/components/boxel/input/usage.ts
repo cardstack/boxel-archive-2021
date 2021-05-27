28fd94c6ac18466c4014a6db8d2961607fb9736e
Burcu Noyan <burcunoyan@gmail.com>
2021-05-27 15:28:16 -0400

    Input field optional and error states styling

--- addon/components/boxel/input/usage.ts
+++ addon/components/boxel/input/usage.ts
@@ -1,9 +1,26 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class extends Component {
   @tracked id = 'sample-input';
-  @tracked value = 'Hello Boxel';
+  @tracked value = '';
   @tracked disabled = false;
   @tracked required = false;
+  @tracked optional = false;
+  @tracked errorMessage = '';
+  @tracked helperText = '';
+
+  @action validate(ev: Event): void {
+    let target = ev.target as HTMLInputElement;
+    if (!target.validity?.valid) {
+      if (target.validity?.valueMissing) {
+        this.errorMessage = 'This is a required field';
+      } else {
+        this.errorMessage = target.validationMessage;
+      }
+      return;
+    }
+    this.errorMessage = '';
+  }
 }
